450cacd40902c2dc3b54483375d01883f6a5b24c
Ville Koskinen <ville.koskinen94@hotmail.fi>
2016-04-18 10:29:50 +0300

    Physics pohjat
    ebin redi nyt vaan ku ois world ebin ebin

--- Engine/Engine/Physics.h
+++ Engine/Engine/Physics.h
@@ -1,11 +1,26 @@
+#include "SimulationWorld.h"
+#include <glm.hpp>
+
 #ifndef PHYSICS_H
 #define PHYSICS_H
 
-class Physics
+//Simulated world, size 400 x 400
+SimulationWorld world(400, 400);
+
+//Gravity, x = 0 and y = -9.81 as per normal
+glm::vec2 gravity(0.0f, -9.81f);
+
+void Run()
 {
-public:
-	Physics();
-	~Physics();
-};
+    static float lastTime;
+    float time = lastTime + 0.02f;
+
+    //world.simulate(time - lastTime);
+    //world.render();
+
+    lastTime = time;
+}
+
+
 
 #endif
