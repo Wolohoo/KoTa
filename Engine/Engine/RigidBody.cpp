4d9973484cb9e585742438463ec3008a3103553a
Niko Tapani Tauriainen <niko.tauriainen@hotmail.com>
2016-04-18 09:56:20 +0300

    glm and rogidbody
    asdasda

--- Engine/Engine/RigidBody.cpp
+++ Engine/Engine/RigidBody.cpp
@@ -1,11 +1,36 @@
 #include "RigidBody.h"
 
 
-RigidBody::RigidBody()
+RigidBody::RigidBody(float width, float heigth, float mass, float inertia)
 {
+    m_width = width;
+    m_height = heigth;
+    m_mass = mass;
+    m_inertia = inertia;
 }
 
 
 RigidBody::~RigidBody()
 {
 }
+
+void RigidBody::setAngVel(glm::vec2 angularVelocity)
+{
+    m_angularVelocity;
+}
+
+glm::vec2 RigidBody::getAngVel()
+{
+    return m_velocity;
+}
+
+glm::vec2 RigidBody::getTorque()
+{
+    return m_force;
+}
+
+glm::vec2 RigidBody::getOrientation()
+{
+    return m_position;
+}
+
